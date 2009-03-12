b61b04d4f6cd5fd290d0ce28a0fafcf89088fb78
Michael Fogleman <fogleman@gmail.com>
2009-03-12 03:30:35 +0000

    pybrowser.py

--- pybrowser.py
+++ pybrowser.py
@@ -1,3 +1,6 @@
+import wx
+import util
+
 class Node(object):
     ROOT = 0
     CLASS = 1
@@ -57,8 +60,53 @@ class Parser(object):
                 stack.append(node)
         return root
         
+class Control(wx.TreeCtrl):
+    def __init__(self, parent, root=None):
+        super(Control, self).__init__(parent, -1, style=wx.TR_HIDE_ROOT|wx.TR_FULL_ROW_HIGHLIGHT|wx.TR_NO_LINES)
+        self.set_root(root)
+        font = self.GetFont()
+        font.SetFaceName('Bitstream Vera Sans Mono')
+        font.SetPointSize(10)
+        self.SetFont(font)
+        images = wx.ImageList(16, 16, False)
+        images.Add(util.get_icon('bullet_go.png'))
+        images.Add(util.get_icon('bullet_orange.png'))
+        self.SetImageList(images)
+        self.images = images
+        self.Bind(wx.EVT_TREE_SEL_CHANGED, self.on_sel_changed)
+    def on_sel_changed(self, event):
+        item = self.GetSelection()
+        data = self.GetPyData(item)
+        print data.line
+    def set_root(self, node):
+        self.DeleteAllItems()
+        if not node: return
+        root = self.AddRoot('')
+        top = None
+        for i, child in enumerate(node.children):
+            item = self.add_node(root, child)
+            self.Expand(item)
+            if i == 0: top = item
+        if top:
+            self.SelectItem(top)
+    def add_node(self, parent, node):
+        item = self.AppendItem(parent, node.text)
+        self.SetPyData(item, node)
+        if node.type == Node.CLASS:
+            self.SetItemImage(item, 0)
+            self.SetItemBold(item)
+        elif node.type == Node.DEF:
+            self.SetItemImage(item, 1)
+        for child in node.children:
+            self.add_node(item, child)
+        return item
+        
 if __name__ == '__main__':
     parser = Parser()
     result = parser.parse_file('pybrowser.py')
-    result.display()
+    app = wx.PySimpleApp()
+    frame = wx.Frame(None, -1, 'Python Class Browser')
+    control = Control(frame, result)
+    frame.Show()
+    app.MainLoop()
     
